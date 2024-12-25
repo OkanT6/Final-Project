58f5800a01ada5b74dd130524d9a26c7f8774c36
Okan Topdemir <149389691+OkanT6@users.noreply.github.com>
2024-12-25 16:07:57 +0300

    Delete DTOs directory

--- DTOs/BransDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace EruKampusSpor.DTOs
-{
-    public class BransDTO
-    {
-        public int BransId { get; set; }
-        public string BransAdı { get; set; }
-    }
-}
--- DTOs/TesisDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace EruKampusSpor.DTOs
-{
-    public class TesisDTO
-    {
-        public int TesisId { get; set; } //Primary Key
-        public string TesisAdı { get; set; }
-    }
-}
